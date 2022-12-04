692815a9f85027596f4aa0d92eacdc15773988b3
cperotti <carolinaperotti16@gmail.com>
2022-12-04 16:25:50 -0300

    proyecto vue

---
+++ src/App.vue
@@ -0,0 +1,111 @@
+<template>
+  <div id="app" class="row">
+    <h2 class="titulo">{{titulo}}</h2> 
+    <table-component v-for="(producto, key) in productos" :key="key" :columnas="columnas" :producto="producto" :estilo="estilos[key]" />
+  </div>
+</template>
+
+<script>
+import TableComponent from './components/tableComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TableComponent
+  },
+  data(){
+    return{ 
+      titulo:'Componentes vue cdn a vue cli - Carolina Perotti',
+      estilos:[
+        {
+          "table-danger":true,
+          "text-danger":true,
+        },
+        {
+          "table-success":true,
+          "text-success":true,
+        },
+        {
+          "table-warning":true,
+          "text-warning":true,
+        }
+      ],
+      columnas:[
+        {
+          columna:'Tipo',
+        },
+        {
+          columna:'Precio',
+        },
+      ],
+      productos:[
+        {
+          tipo:'Carnes',
+          variedades:[
+            {
+              nombre:'Vacio',
+              precio: 1300,
+            },
+            {
+              nombre:'Nalga',
+              precio: 1400,
+            },
+            {
+              nombre:'Costilla',
+              precio: 1099,
+            }
+          ]  
+        },
+        {
+          tipo:'Vegetales',
+          variedades:[
+            {
+              nombre:'Cebollas',
+              precio: 200,
+            },
+            {
+              nombre:'Rucula',
+              precio: 90,
+            },
+            {
+              nombre:'Tomate',
+              precio: 170,
+            },
+          ]
+        },
+        {
+          tipo:'Bebidas',
+          variedades:[
+            {
+              nombre:'Cerveza',
+              precio:700
+            },
+            {
+              nombre:'Vino',
+              precio:800
+            },
+            {
+              nombre:'Gin',
+              precio:1800
+            },
+          ]
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 30px;
+}
+.titulo{
+  margin-bottom: 30px;
+}
+</style>
---
+++ src/components/tableComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="col">
+        <h4>{{producto.tipo}}</h4>
+        <table :class='estilo' class="table table-bordered">
+            <thead>
+                <tr>
+                    <th v-for='(columna, key) in columnas' :key='key'>{{columna.columna}}
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for='(variedad, key) in producto.variedades' :key='key'>
+                    <td>{{variedad.nombre}}</td>
+                    <td>$ {{variedad.precio}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'tableComponent',
+        props:{
+            columnas: Array,
+            producto: Object,
+            estilo: Object,
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
